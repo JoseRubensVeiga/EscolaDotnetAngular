b0e577c9b22ddb41e1ae432f4102ad568be6edc0
José <jose.rubens.veiga@gmail.com>
2021-03-26 22:56:25 -0300

    finalizando criação do header e sidenav

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { SidenavComponent } from './sidenav';
 import { TopbarComponent } from './topbar';
 
 @NgModule({
   declarations: [SidenavComponent, TopbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   exports: [SidenavComponent, TopbarComponent],
 })
 export class SharedModule {}
